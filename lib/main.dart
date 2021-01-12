2bf0742ffa348e0241b340b2cf02b455c230f207
DimaMamaev <dmamaev@my.com>
2021-01-12 11:57:18 +0200

    define orientation

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,13 @@ import 'package:crud/components/transactionsEmpty.dart';
 import 'package:crud/components/transactionsInput.dart';
 import 'package:crud/model/transaction.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp, DeviceOrientation.portraitDown])
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
